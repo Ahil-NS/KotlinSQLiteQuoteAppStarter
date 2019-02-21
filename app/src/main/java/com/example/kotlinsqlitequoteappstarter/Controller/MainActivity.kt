707d50d5d7eafc61e6f0233fce19d1cebc842694
Ahil <eahilendran@gmail.com>
2019-02-21 22:11:12 +0530

    save Quote to Database

--- app/src/main/java/com/example/kotlinsqlitequoteappstarter/Controller/MainActivity.kt
+++ app/src/main/java/com/example/kotlinsqlitequoteappstarter/Controller/MainActivity.kt
@@ -1,28 +1,48 @@
 package com.example.kotlinsqlitequoteappstarter.Controller
 
+import android.app.ProgressDialog
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.text.TextUtils
+import android.view.View
+import android.widget.Toast
 import com.example.kotlinsqlitequoteappstarter.Data.QuoteDatabaseHandler
 import com.example.kotlinsqlitequoteappstarter.Model.Quote
 import com.example.kotlinsqlitequoteappstarter.R
-import java.util.ArrayList
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     var dbHandler: QuoteDatabaseHandler? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         dbHandler = QuoteDatabaseHandler(this)
 
-        var quote = Quote()
-        quote.quoteTitle = "Develop your skills"
-        quote.createdBy = "Ahil"
-        dbHandler!!.createQuote(quote)
 
-        var readQ: Quote = dbHandler!!.readQuote(1)
-        Log.d("DATA INSERTED", "SUCCESS ${readQ.quoteTitle}")
+        getStarterButton.setOnClickListener {
+            progressBar.visibility = View.VISIBLE
+
+            if (!TextUtils.isEmpty(quoteText.text.toString())
+                && !TextUtils.isEmpty(quoteUserText.text.toString())
+                ) {
+                //save to database
+                var quote = Quote()
+                quote.quoteTitle = quoteText.text.toString()
+                quote.createdBy = quoteUserText.text.toString()
+
+                saveToDB(quote)
+                progressBar.visibility = View.GONE
+
+            } else {
+                Toast.makeText(this, "Please enter a quote", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    fun saveToDB(quote: Quote) {
+        dbHandler!!.createQuote(quote)
     }
 }
